3de4a2c353dc9b859534594683051f4a9214734c
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2022-08-25 13:26:07 +0200

    update hero section

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,8 +1,10 @@
+// import "./HeroSection.css";
+import HeroImg from "../../assets/images/banner.svg";
 const HeroSection = () => {
   return (
-    <section className="bg-lightCofee rounded-xl p-5 min-h-[15rem] flex flex-col justify-center">
+    <section className="bg-lightCofee rounded-xl p-5 min-h-[15rem] flex flex-col-reverse md:flex-row justify-around items-center gap-8 py-8">
       <div className="md:max-w-[40%] lg:max-w-[30%]">
-        <h2 className="text-3xl font-black text-darkViolet">
+        <h2 className="text-4xl font-black text-lightViolet">
           Build your library
         </h2>
         <p className="text-gray-500 mt-3">
@@ -11,6 +13,9 @@ const HeroSection = () => {
         <button className="bg-yellow-800 px-3 py-1 rounded-lg mt-5">
           View all
         </button>
+      </div>
+      <div>
+        <img src={HeroImg} width="300px" height="300px" alt="" />
       </div>
     </section>
   );
